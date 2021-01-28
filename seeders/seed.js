4a67e44f329e55e16d913cc6f8b625d33fdb5265
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-28 02:56:51 -0500

    Trouble-shooting heroku deployment

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,8 +3,6 @@ let db = require("../models");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
